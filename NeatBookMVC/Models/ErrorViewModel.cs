2c44d921772b007b48439539989f765dac0f54cf
Максим Микитюк <maks.mykytyuk@gmail.com>
2023-10-06 13:47:50 +0300

    Add project files.

---
+++ NeatBookMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NeatBookMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
